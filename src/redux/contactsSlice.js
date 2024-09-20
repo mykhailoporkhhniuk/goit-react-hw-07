5dfa2395e0d307cd3603a418990c7e8efa088c18
Mykhailo Porokhniuk <mporoh616@gmail.com>
2024-09-20 16:24:38 +0300

    update contactsSlice

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -43,7 +43,7 @@ const contactsSlice = createSlice({
             })
             .addCase(deleteContact.fulfilled, (state, action) => {
                 state.loading = false;
-                state.items.filter((contact) => contact.id !== action.payload.id);
+                state.items = state.items.filter((contact) => contact.id !== action.payload.id);
             })
             .addCase(deleteContact.rejected, (state, action) => {
                 state.loading = false;
